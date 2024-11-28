f5d44c160f5c01c628d42469bd9fe622d86a4188
gustsilveira <gustavosilveira95@hotmail.com>
2024-11-28 00:37:00 -0300

    redeploy app

--- challenge/obj/Debug/net8.0/challenge.AssemblyInfo.cs
+++ challenge/obj/Debug/net8.0/challenge.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("challenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02e9c934186acb61771c818dd57658411236949d")]
 [assembly: System.Reflection.AssemblyProductAttribute("challenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("challenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
